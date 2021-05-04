e6e0fd46a9514d696fc93f28f4ec871f3e3dda7f
Zoid <brandincurry@gmail.com>
2021-05-04 19:26:53 -0400

    basic driver setup

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import json
 
 #text colors
 class bcolors:
@@ -30,3 +30,33 @@ def writeLog(message, type):
           warnings+=1
      elif type.upper() == "INFO" and loggingLevel >= 3:
           print("\033[1;37;40mINFO:",message,bcolors.ENDC)
+
+#read ./settings.json
+with open("dev.settings.json") as settingsFile: #!!!CHANGE THIS BACK TO DEFAULT TO settings.json!!!
+     settings = json.load(settingsFile)
+
+try:
+     item = settings["item"]
+     logLevel = settings["loggingLevel"]
+     email = settings["email"]
+     pwd = settings["password"]
+     secCode = settings["cvv"]
+except:
+     writeLog("Failed to load settings","ERROR")
+     exit()
+
+driver = webdriver.Chrome("chromedriver.exe")
+driver.get(item)
+cardBought = False
+while not cardBought:
+     #find add to cart button
+     try:
+          atcBtn = WebDriverWait(driver,10).until(
+               EC.element_to_be_clickable((By.CSS_SELECTOR,".add-to-cart-button"))
+          )
+     except:
+          writeLog("Could not find clickable ATC button","WARNING")
+          driver.refresh()
+          continue
+
+     writeLog("Add To Cart button found!","INFO")
---
+++ readme.md
@@ -0,0 +1,12 @@
+# ShopPyBot
+
+A quick python project to try to grab targetted items in the competitive resellers market.
+
+## Requirements
+
+- [Python](https://www.python.org/downloads/)
+- Selenium
+     - `pip install selenium`
+- [Google Chrome](https://chrome.google.com)
+- [ChromeDriver](https://chromedriver.chromium.org/downloads)
+     - Drop this in the same directory as `bot.py`
