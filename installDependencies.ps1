202cc4eff889e2fd5ecb41fcde38351b13142c29
Zoid <brandincurry@gmail.com>
2021-05-04 21:54:59 -0400

    fixed alias ps potential issue

--- installDependencies.ps1
+++ installDependencies.ps1
@@ -1,8 +1,8 @@
-start "https://chrome.google.com"
+Start-Process "https://chrome.google.com"
 Read-Host "Press enter to continue once you download and install Google Chrome"
-start "https://chromedriver.chromium.org/downloads"
+Start-Process "https://chromedriver.chromium.org/downloads"
 Read-Host "Press enter to continue once you download and move chromedriver.exe"
-start "https://www.python.org/downloads/"
+Start-Process "https://www.python.org/downloads/"
 Read-Host "Press enter to continue once you download and install Python 3.9 (add to PATH and include PIP install)"
 pip install selenium
 pip install playsound
