7b1cb464c81278975e833436debcebc874e0559c
Zoid <brandincurry@gmail.com>
2021-05-04 21:45:34 -0400

    updated ps dependencies script

--- installDependencies.ps1
+++ installDependencies.ps1
@@ -1,7 +1,10 @@
-pip install selenium
-pip install playsound
 start "https://chrome.google.com"
 Read-Host "Press enter to continue once you download and install Google Chrome"
 start "https://chromedriver.chromium.org/downloads"
 Read-Host "Press enter to continue once you download and move chromedriver.exe"
+start "https://www.python.org/downloads/"
+Read-Host "Press enter to continue once you download and install Python 3.9 (add to PATH and include PIP install)"
+pip install selenium
+pip install playsound
+
 Write-host -ForegroundColor Green "Dependencies should be installed if no errors were encountered above"
